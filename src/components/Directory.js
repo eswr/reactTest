7e40b84a6a1a0e3e63d8e5d916810f4584c07b75
tusharendra <tusharendra.k@gmail.com>
2021-01-17 02:22:10 +0530

    added db

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/styles'
 import cx from 'classnames'
 
@@ -17,55 +17,15 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-const defaultDirectory = [
-  {
-    path: '.',
-    type: 'folder',
-    name: 'src',
-    isOpen: false,
-    subDirectory: [
-      {
-        path: './src',
-        type: 'folder',
-        name: 'component',
-        isOpen: false,
-        subDirectory: [
-          {
-            path: './src/component',
-            type: 'folder',
-            name: 'component',
-            isOpen: false,
 
-          },
-        ]
-      },
-      {
-        path: './src',
-        type: 'folder',
-        name: 'container',
-        isOpen: false,
-      },
-      {
-        path: './src',
-        type: 'file',
-        name: 'App.js',
-      },
-    ]
-  },
-  {
-    path: '.',
-    type: 'file',
-    name: 'package.js',
-  },
-]
 
-const FileSystem = ({ directory, toggleExpand }) => {
+const FileSystem = ({ directory, toggleExpand, onFileClick }) => {
   const classes = useStyles()
 
   return (
     <ul className={classes.wrapper}>
       {directory.map(d => (<div key={d.path + '/' + d.name}>
-        <li onClick={() => toggleExpand(d)}>
+        <li onClick={() => d.type === "file" ? onFileClick(d) : toggleExpand(d)}>
           {d.type === 'folder' && (d.isOpen ? (
             <span className={classes.icon}>+</span>
           ) : (
@@ -76,14 +36,21 @@ const FileSystem = ({ directory, toggleExpand }) => {
             {d.name}
           </span>
         </li>
-        {d.subDirectory && (d.isOpen && <FileSystem directory={d.subDirectory} toggleExpand={toggleExpand} />)}
+        {d.subDirectory && (d.isOpen && <FileSystem directory={d.subDirectory} toggleExpand={toggleExpand} onFileClick={onFileClick}/>)}
       </div>))}
     </ul>
   )
 }
 
-function Directory() {
-  const [directory, setDirectory] = useState(defaultDirectory)
+
+function Directory({setDirectory, directory, onFileClick }) {
+  
+
+  
+
+
+ 
+
 
   const matchAndToggle = (dir, partialPath, d) => dir.map(i => {
     if (i.name === partialPath[0]){
@@ -106,13 +73,15 @@ function Directory() {
   const deleteDirectory = () => { debugger }
 
 
+
+
   return (
     <div>
       <div>
         <button onClick={addDirectory}>Add</button>
         <button onClick={deleteDirectory}>Del</button>
       </div>
-      <FileSystem directory={directory} toggleExpand={toggleExpand} />
+      <FileSystem directory={directory} toggleExpand={toggleExpand} onFileClick={onFileClick}/>
     </div>
   )
 }
